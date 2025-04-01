a3212aaabd5ee664995a841dcc6891495231b403
To'Iki Rouillard <trouilla@shi-r5-p4.s19.be>
2025-04-01 11:49:20 +0200

    Norminette done for all except parsing and almost point.h

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: trouilla <trouilla@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 10:37:51 by sinawara          #+#    #+#             */
-/*   Updated: 2025/04/01 11:44:24 by trouilla         ###   ########.fr       */
+/*   Updated: 2025/04/01 11:48:35 by trouilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 # include "../libft/include/ft_printf.h"
 # include "../libft/include/get_next_line.h"
 # include "../libft/include/libft.h"
-# include <sys/time.h>
-# include <stdio.h>
-# include <string.h>
-# include <unistd.h>
+# include <ctype.h>
+# include <fcntl.h>
 # include <math.h>
 # include <mlx.h>
-# include <fcntl.h>
-# include <ctype.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
 # include <time.h>
+# include <unistd.h>
 
 /* Key codes for events */
 # define KEY_ESC 65307
@@ -49,50 +49,50 @@
 # define FRAME_TIME (1000000 / FPS)
 
 /* minimap constant*/
-#define MINIMAP_SIZE 200
-#define MINIMAP_SCALE 10
-#define MINIMAP_MARGIN 20
-#define MINIMAP_ALPHA 0.7
+# define MINIMAP_SIZE 200
+# define MINIMAP_SCALE 10
+# define MINIMAP_MARGIN 20
+# define MINIMAP_ALPHA 0.7
 
 /* Existing texture structure */
 typedef struct s_textures
 {
-	int		is_no;
-	int		is_so;
-	int		is_ea;
-	int		is_we;
-	int		is_f;
-	int		is_c;
-	char	*path_no;
-	char	*path_so;
-	char	*path_ea;
-	char	*path_we;
-	char	*color_f;
-	int		*color_f_array;
-	char	*color_c;
-	int		*color_c_array;
-}	t_textures;
+	int			is_no;
+	int			is_so;
+	int			is_ea;
+	int			is_we;
+	int			is_f;
+	int			is_c;
+	char		*path_no;
+	char		*path_so;
+	char		*path_ea;
+	char		*path_we;
+	char		*color_f;
+	int			*color_f_array;
+	char		*color_c;
+	int			*color_c_array;
+}				t_textures;
 
 typedef struct s_keys
 {
-	int		w;
-	int		a;
-	int		s;
-	int		d;
-	int		left;
-	int		right;
-}	t_keys;
+	int			w;
+	int			a;
+	int			s;
+	int			d;
+	int			left;
+	int			right;
+}				t_keys;
 /* MLX image structure */
 typedef struct s_img
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	int		width;
-	int		height;
-}	t_img;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	int			width;
+	int			height;
+}				t_img;
 
 /* Map structure */
 /* typedef struct s_map
@@ -105,73 +105,73 @@ typedef struct s_img
 /* Structure for rectangle coordinates and size */
 typedef struct s_rect
 {
-	int	x;
-	int	y;
-	int	size;
-}	t_rect;
+	int			x;
+	int			y;
+	int			size;
+}				t_rect;
 
 /* Player structure */
 typedef struct s_player
 {
-	double	pos_x;
-	double	pos_y;
-	double	dir_x;
-	double	dir_y;
-	char	direction;
-}	t_player;
+	double		pos_x;
+	double		pos_y;
+	double		dir_x;
+	double		dir_y;
+	char		direction;
+}				t_player;
 
 /* Camera structure */
 typedef struct s_camera
 {
-	double	plane_x;
-	double	plane_y;
-}	t_camera;
+	double		plane_x;
+	double		plane_y;
+}				t_camera;
 
 /* Ray structure */
 typedef struct s_ray
 {
-	double	dir_x;
-	double	dir_y;
-	int		map_x;
-	int		map_y;
-	double	side_dist_x;
-	double	side_dist_y;
-	double	delta_dist_x;
-	double	delta_dist_y;
-	double	perp_wall_dist;
-	int		step_x;
-	int		step_y;
-	int		side;
-	int		hit;
-	int		line_height;
-	int		draw_start;
-	int		draw_end;
-}	t_ray;
+	double		dir_x;
+	double		dir_y;
+	int			map_x;
+	int			map_y;
+	double		side_dist_x;
+	double		side_dist_y;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		perp_wall_dist;
+	int			step_x;
+	int			step_y;
+	int			side;
+	int			hit;
+	int			line_height;
+	int			draw_start;
+	int			draw_end;
+}				t_ray;
 
 typedef struct s_time
 {
-	long	old_time;
-	long	current_time;
-	double	frame_time;
-}	t_time;
+	long		old_time;
+	long		current_time;
+	double		frame_time;
+}				t_time;
 
 /* Texture position for rendering */
 typedef struct s_tex_pos
 {
-	t_img	*img;
-	int		x;
-	int		y;
-	double	pos;
-	double	step;
-}	t_tex_pos;
-
+	t_img		*img;
+	int			x;
+	int			y;
+	double		pos;
+	double		step;
+}				t_tex_pos;
 
 // New map structure
-typedef struct s_map {
-    char **map;      // 2D array of map characters
-    int height;      // Number of rows
-    int width;       // Max width of map
-} t_map;
+typedef struct s_map
+{
+	char		**map;
+	int			height;
+	int			width;
+}				t_map;
 
 /* Main game structure */
 typedef struct s_game
@@ -192,119 +192,119 @@ typedef struct s_game
 	char		*map_path;
 	t_keys		keys;
 	t_time		time;
-}	t_game;
-
-
+}				t_game;
 
 // file_check.c //
-int is_cub_file(const char *filename);
-int is_xpm_file(const char *filename);
-int check_permission(const char *filename);
-int check_textures(const char *line, int i, t_textures *textures);
-t_textures  *init_textures(void);
-int textures_present(t_textures *textures);
+int				is_cub_file(const char *filename);
+int				is_xpm_file(const char *filename);
+int				check_permission(const char *filename);
+int				check_textures(const char *line, int i, t_textures *textures);
+t_textures		*init_textures(void);
+int				textures_present(t_textures *textures);
 
-//main.c//
-int key_press(int keycode, t_game *game);
-int	check_validation_result(t_game *game, int result);
-int load_map(t_game *game, const char *filename);
-int validate_inputs(int argc, char **argv);
+// main.c//
+int				key_press(int keycode, t_game *game);
+int				check_validation_result(t_game *game, int result);
+int				load_map(t_game *game, const char *filename);
+int				validate_inputs(int argc, char **argv);
 
 // parse_files.c //
-int	skip_whitespace(char *line);
-int	process_config_line(t_game *game, char *line, int fd);
-int parse_file(t_game *game, const char *filename);
+int				skip_whitespace(char *line);
+int				process_config_line(t_game *game, char *line, int fd);
+int				parse_file(t_game *game, const char *filename);
 
-//init.c//
-void init_game(t_game *game);
-int init_mlx(t_game *game);
-void init_player(t_game *game);
-void setup_player_direction(t_game *game);
-int	load_texture(t_game *game, t_img *texture, char *path);
+// init.c//
+void			init_game(t_game *game);
+int				init_mlx(t_game *game);
+void			init_player(t_game *game);
+void			setup_player_direction(t_game *game);
+int				load_texture(t_game *game, t_img *texture, char *path);
 
 // init_utils.c //
-void init_games(t_game *game);
-int parse_color(char *color_str);
-int	load_textures(t_game *game);
-void direction_north(t_game *game);
+void			init_games(t_game *game);
+int				parse_color(char *color_str);
+int				load_textures(t_game *game);
+void			direction_north(t_game *game);
 
-//cleanup.c//
-void    free_map(t_map *map);
-void free_textures(t_game *game);
-void    free_images(t_game *game);
-int	clean_exit(t_game *game, int status);
-int	error_handle(t_game *game, char *error_message);
-void free_split(char **split);
+// cleanup.c//
+void			free_map(t_map *map);
+void			free_textures(t_game *game);
+void			free_images(t_game *game);
+int				clean_exit(t_game *game, int status);
+int				error_handle(t_game *game, char *error_message);
+void			free_split(char **split);
 
-//check_map.c//
-int is_map_line(const char *line);
-int is_texture_line(const char *line);
-char **allocate_map(int rows, int cols);
-char **extract_map(int fd, int *rows, int *cols);
-int validate_map_structure(const char *filename);
+// check_map.c//
+int				is_map_line(const char *line);
+int				is_texture_line(const char *line);
+char			**allocate_map(int rows, int cols);
+char			**extract_map(int fd, int *rows, int *cols);
+int				validate_map_structure(const char *filename);
 
-//color_check.c//
-int is_valid_rgb_component(const char *str);
-int *rgb_split(const char *rgb_str);
+// color_check.c//
+int				is_valid_rgb_component(const char *str);
+int				*rgb_split(const char *rgb_str);
 
-//parse_map.c//
-int flood_fill(char **map, int x, int y, int rows, int cols);
-int is_map_enclosed(char **map, int rows, int cols);
-int validate_map_content(char **map, int rows, int cols);
-int validate_map(char **map, int rows, int cols);
-char	**duplicate_map(char **map, int rows, int cols);
+// parse_map.c//
+int				flood_fill(char **map, int x, int y, int rows, int cols);
+int				is_map_enclosed(char **map, int rows, int cols);
+int				validate_map_content(char **map, int rows, int cols);
+int				validate_map(char **map, int rows, int cols);
+char			**duplicate_map(char **map, int rows, int cols);
 
-// free.c // 
-void	free_map_copy(char **map_copy, int rows);
-int handle_exit(t_game *game);
+// free.c //
+void			free_map_copy(char **map_copy, int rows);
+int				handle_exit(t_game *game);
 
-//raycasting.c//
-void cast_rays(t_game *game);
-void calculate_ray(t_game *game, t_ray *ray, int x);
-void	init_dda_x(t_game *game, t_ray *ray);
-void	init_dda_y(t_game *game, t_ray *ray);
-int	check_ray_hit(t_game *game, t_ray *ray);
-void perform_dda(t_game *game, t_ray *ray);
-void calculate_wall_distance(t_ray *ray, t_game *game);
-void calculate_wall_height(t_ray *ray, int screen_height);
-void draw_wall_stripe(t_game *game, int x, t_ray *ray);
-void	draw_ceiling(t_game *game, int x, t_ray *ray);
-void	draw_floor(t_game *game, int x, t_ray *ray);
-void	draw_textured_wall(t_game *game, int x, t_ray *ray, t_tex_pos *tex);
-void	calculate_texture_position(t_game *game, t_ray *ray, t_tex_pos *tex);
-int get_pixel_color(t_img *img, int x, int y);
-void put_pixel(t_img *img, int x, int y, int color);
-void clear_image(t_img *img, int color);
+// raycasting.c//
+void			cast_rays(t_game *game);
+void			calculate_ray(t_game *game, t_ray *ray, int x);
+void			init_dda_x(t_game *game, t_ray *ray);
+void			init_dda_y(t_game *game, t_ray *ray);
+int				check_ray_hit(t_game *game, t_ray *ray);
+void			perform_dda(t_game *game, t_ray *ray);
+void			calculate_wall_distance(t_ray *ray, t_game *game);
+void			calculate_wall_height(t_ray *ray, int screen_height);
+void			draw_wall_stripe(t_game *game, int x, t_ray *ray);
+void			draw_ceiling(t_game *game, int x, t_ray *ray);
+void			draw_floor(t_game *game, int x, t_ray *ray);
+void			draw_textured_wall(t_game *game, int x, t_ray *ray,
+					t_tex_pos *tex);
+void			calculate_texture_position(t_game *game, t_ray *ray,
+					t_tex_pos *tex);
+int				get_pixel_color(t_img *img, int x, int y);
+void			put_pixel(t_img *img, int x, int y, int color);
+void			clear_image(t_img *img, int color);
 
-//move.c//
-void move_player(t_game *game, double move_speed);
-void strafe_player(t_game *game, double strafe_speed);
-void	adjust_camera_plane(t_player *player, t_camera *camera);
-void	normalize_dir_vector(t_player *player);
-void rotate_player(t_game *game, double rot_speed);
+// move.c//
+void			move_player(t_game *game, double move_speed);
+void			strafe_player(t_game *game, double strafe_speed);
+void			adjust_camera_plane(t_player *player, t_camera *camera);
+void			normalize_dir_vector(t_player *player);
+void			rotate_player(t_game *game, double rot_speed);
 
 // move_utils.c //
-int key_press(int keycode, t_game *game);
-int	key_state_handler(t_game *game);
-int	key_release_state(int keycode, t_game *game);
-int	key_press_state(int keycode, t_game *game);
-int is_valid_move(t_game *game, double x, double y);
+int				key_press(int keycode, t_game *game);
+int				key_state_handler(t_game *game);
+int				key_release_state(int keycode, t_game *game);
+int				key_press_state(int keycode, t_game *game);
+int				is_valid_move(t_game *game, double x, double y);
 
-//time.c//
-void	init_time(t_game *game);
-void	update_time(t_game *game);
-double	get_frame_time(t_game *game);
-void	frame_delay(t_game *game);
+// time.c//
+void			init_time(t_game *game);
+void			update_time(t_game *game);
+double			get_frame_time(t_game *game);
+void			frame_delay(t_game *game);
 
 // game_loop.c //
-void	init_key_states(t_game *game);
-int	game_loop(t_game *game);
-int	game_update(t_game *game);
-int	render_frame(t_game *game);
-void process_movement(t_game *game, double time_factor);
-void test_player_movement(t_game *game, int frame_count);
+void			init_key_states(t_game *game);
+int				game_loop(t_game *game);
+int				game_update(t_game *game);
+int				render_frame(t_game *game);
+void			process_movement(t_game *game, double time_factor);
+void			test_player_movement(t_game *game, int frame_count);
 
 // fps.c //
-void	draw_fps_counter(t_game *game);
+void			draw_fps_counter(t_game *game);
 
 #endif
